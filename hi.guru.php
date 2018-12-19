b547e7dc38fffcdc73c96123cf86e0b08a366852
bennie-higuru <bennie@hi.guru>
2018-12-19 12:34:11 +0200

    added bots in head tags for hi.guru and consultaPanel FAQ

--- hi.guru.php
+++ hi.guru.php
@@ -19,6 +19,19 @@
   <!-- Custom styles for this template -->
   <link href="css/style.css" rel="stylesheet">
 
+  <!-- Hi.guru bot -->
+  <script src="https://s3-eu-west-1.amazonaws.com/higuru-webchat-stage/v1/higuru-webchat-stage.min.js"></script>
+  <script>
+    (function() {
+    document.addEventListener("DOMContentLoaded", function(){
+    var body = document.body;
+    var higuruChatBlock = document.createElement('div');
+    higuruChatBlock.setAttribute('id', 'higuru-webchat');
+    body.appendChild(higuruChatBlock);
+    Higuru.WebChat.init({ selector: "#higuru-webchat", token: '41f8d047-c2ff-481e-a550-4957471986dd'});
+    });
+    })();
+  </script>
 </head>
 
 <body id="page-top">
